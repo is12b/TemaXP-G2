fd8d26b2a8f26b58d9c72731101f5b1c11974946
is12b <zoinoz@gmail.com>
2015-09-03 13:42:46 +0200

    Resharper settings

--- WCFBusinessLogic/Class1.cs
+++ WCFBusinessLogic/Class1.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WCFBusinessLogic
-{
-    public class Class1
-    {
+namespace WCFBusinessLogic {
+
+    
+
+    public class Class1 {
+
+        
+        
     }
+
 }
