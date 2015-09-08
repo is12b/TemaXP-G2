ca6d7350899781e67d1688f70c11c0dc4ca58254
Bo Stokholm <info@stokholmsolutions.dk>
2015-09-08 11:32:41 +0200

    Wcf tests

---
+++ WCFUnitTest/ArtPieceServiceTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WCFUnitTest.MainServiceReference;
+
+namespace WCFUnitTest {
+    [TestClass]
+    public class ArtPieceServiceTest {
+
+        MainServiceReference.IArtPieceService _artPieceService = new ArtPieceServiceClient();
+        [TestMethod]
+        public void AddArtPieceTest() {
+
+            try {
+                _artPieceService.AddArtPiece(new ArtPiece() {
+                    Artist = "%/!",
+                    Name = "/&()",
+                    Description = "&/!",
+                    Number = 1,
+                    PictureUrl = "www.pornhub.com",
+                    PurchasePrice = 12
+                });
+                Assert.Fail("Regex failed #ArtPiece");
+            } catch (Exception e) {
+                Assert.IsTrue(true);
+            }
+
+        }
+
+        [TestMethod]
+        public void UpdateArtPieceTest() {
+            try {
+                _artPieceService.UpdateArtPiece(new ArtPiece() {
+                    Artist = "%/!",
+                    Name = "/&()",
+                    Description = "&/!",
+                    Number = 1,
+                    PictureUrl = "www.pornhub.com",
+                    PurchasePrice = 12
+                });
+                Assert.Fail("Regex failed on Update");
+            } catch (Exception e) {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
---
+++ WCFUnitTest/AuctionServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WCFUnitTest.MainServiceReference;
+
+namespace WCFUnitTest {
+    [TestClass]
+    public class AuctionServiceTest {
+        MainServiceReference.IAuctionService _auctionService = new AuctionServiceClient();
+        [TestMethod]
+        public void AddAuctionTest() {
+            try {
+                _auctionService.AddAuction(new Auction() {
+                    AuctionName = "&!/(#"                    
+                });
+                Assert.Fail("Regex failed");
+            }
+            catch (Exception e) {
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod]
+        public void UpdateAuctionTest() {
+            try {
+                _auctionService.UpdateAuction(new Auction() {
+                    AuctionName = "/!&%"
+                });
+                Assert.Fail("Regex failed on update");
+            }
+            catch (Exception e) {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
