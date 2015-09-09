533e34454c298120f7db1bc24ccbe91f2a3a49ed
Nick D Pedersen <zoinoz@gmail.com>
2015-09-09 13:31:13 +0200

    Fixing tostring overload

--- AdminClient/Model/ArtPiece.cs
+++ AdminClient/Model/ArtPiece.cs
@@ -1,5 +1,9 @@
 ﻿namespace AdminClient.ServiceReference1 {
     public partial class ArtPiece{
         public bool Checked { get; set; }
+
+        public override string ToString() {
+            return this.Name;
+        }
     }
 }
